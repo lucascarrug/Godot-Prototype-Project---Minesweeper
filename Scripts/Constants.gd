c4fe9920cbf15dcb6b7252e94f152749010a7876
lucascarrug <lucascarrizoruggeri2004@gmail.com>
2025-08-30 18:16:58 +0200

    Explore neightbors completed.

--- Scripts/Constants.gd
+++ Scripts/Constants.gd
@@ -4,11 +4,30 @@ class_name Constants extends Resource
 const TILE_SIZE: int = 16
 
 ## Tileset.
-const GREEN_TILE: Vector2i = Vector2i(0,0)
-const DARK_GREEN_TILE: Vector2i = Vector2i(1,0)
-const ORANGE_TILE: Vector2i = Vector2i(2,0)
+const GREEN_TILE: 		Vector2i = Vector2i(0,0)
+const DARK_GREEN_TILE: 	Vector2i = Vector2i(1,0)
+const ORANGE_TILE: 		Vector2i = Vector2i(2,0)
 const DARK_ORANGE_TILE: Vector2i = Vector2i(3,0)
-const FLAG_TILE: Vector2i = Vector2i(4,0)
+const FLAG_TILE: 		Vector2i = Vector2i(4,0)
+const BOMB_TILE: 		Vector2i = Vector2i(5,0)
+const ONE_TILE: 		Vector2i = Vector2i(6,0)
+const TWO_TILE:			Vector2i = Vector2i(0,1)
+const THREE_TILE:		Vector2i = Vector2i(1,1)
+const FOUR_TILE:  		Vector2i = Vector2i(2,1)
+const FIVE_TILE:  		Vector2i = Vector2i(3,1)
+const SIX_TILE:   		Vector2i = Vector2i(4,1)
+const SEVEN_TILE: 		Vector2i = Vector2i(5,1)
+const EIGHT_TILE: 		Vector2i = Vector2i(6,1)
+
+var number_tile = [
+	ONE_TILE,
+	TWO_TILE,
+	THREE_TILE,
+	FOUR_TILE,
+	FIVE_TILE,
+	SIX_TILE,
+	SEVEN_TILE,
+	EIGHT_TILE]
 
 ## Bomb map.
 const BOMB = -1
--- Scripts/Minesweeper.gd
+++ Scripts/Minesweeper.gd
@@ -65,7 +65,6 @@ func set_mines(first_pos: Vector2i) -> void:
 		var x = randi() % C.MAP_SIZE_X
 		var y = randi() % C.MAP_SIZE_Y
 		var new_mine: Vector2i = Vector2i(x,y)
-		print(mines_added, ": New mine: ", new_mine)
 			
 		if not mine_positions.has(new_mine) and new_mine != first_pos: 
 			mine_positions.append(new_mine)
@@ -80,6 +79,7 @@ func set_mines(first_pos: Vector2i) -> void:
 	mines_set = true
 	
 	set_proximity(mine_positions)
+	set_sprite_table()
 
 func set_proximity(mine_positions: Array[Vector2i]) -> void:
 	# Set neightbors.
@@ -98,12 +98,21 @@ func set_proximity(mine_positions: Array[Vector2i]) -> void:
 			if not out_of_map(x,y) and mine_map[y][x] != -1:
 				mine_map[y][x] += 1
 
-
 func out_of_map(x: int, y: int):
 	if x < 0 or x >= C.MAP_SIZE_X or y < 0 or y >= C.MAP_SIZE_Y:
 		return true
 	return false
-	
+
+func set_sprite_table() -> void:
+	for x in C.MAP_SIZE_X:
+		for y in C.MAP_SIZE_Y:
+			if mine_map[y][x] > 0 and mine_map[y][x] <= 8:
+				var atlas_coords = C.number_tile[mine_map[y][x] - 1]
+				#print("minemap[", x, ",", y,"] -> atlas_coords: ", atlas_coords)
+				spritetable.set_cell(Vector2i(x,y), 0, atlas_coords)
+			elif mine_map[y][x] == -1:
+				spritetable.set_cell(Vector2i(x,y), 0, C.BOMB_TILE)
+
 func print_mine_map() -> void:
 	print("MINE MAP")
 	for y in range(0, mine_map.size()):
@@ -119,8 +128,56 @@ func set_exploration_map(max_x: int = C.MAP_SIZE_X, max_y: int = C.MAP_SIZE_Y) -
 		exploration_map.append(col)
 
 func explore(tile_pos: Vector2i) -> void:
-	if exploration_map[tile_pos.y][tile_pos.x] == C.ExplorationMapStates.NOT_EXPLORED:
-		exploration_map[tile_pos.y][tile_pos.x] = C.ExplorationMapStates.EXPLORED
+	var x = tile_pos.x
+	var y = tile_pos.y
+	
+	if exploration_map[y][x] == C.ExplorationMapStates.NOT_EXPLORED:
+		if mine_map[y][x] == 0:
+			explore_neightbors(tile_pos)
+			exploration_map[y][x] == C.ExplorationMapStates.EXPLORED
+			greentable.erase_cell(tile_pos)
+		elif mine_map[y][x] == C.BOMB:
+			explode()
+		else:
+			exploration_map[y][x] == C.ExplorationMapStates.EXPLORED
+			greentable.erase_cell(tile_pos)
+		
+func explore_neightbors(first_neightbor: Vector2i) -> Array[Vector2i]:
+	# Define neightbors.
+	var neighbors := [
+		Vector2i(-1,-1), Vector2i(0,-1), Vector2i(1,-1),
+		Vector2i(-1, 0),                 Vector2i(1, 0),
+		Vector2i(-1, 1), Vector2i(0, 1), Vector2i(1, 1),
+	]
+	
+	# Set incomming and repeated neightbors arrays.
+	var clean_neightbors: Array[Vector2i] = []
+	var seen_neightbors: Array[Vector2i] = []
+	clean_neightbors.append(first_neightbor)
+	
+	# While clean neightbors are not empty.
+	while not clean_neightbors.is_empty():
+		# Check every neightbor.
+		for offset in neighbors:
+			# Get neightbor coords.
+			var x = clean_neightbors[0][0] + offset[0]
+			var y = clean_neightbors[0][1] + offset[1]
+			
+			# Check is in map.
+			if out_of_map(x,y): continue
+			
+			# If neightbor is 0 and not checked before.
+			if mine_map[y][x] == 0 and not seen_neightbors.has(Vector2i(x,y)) and not clean_neightbors.has(Vector2i(x,y)):
+				clean_neightbors.append(Vector2i(x,y))
+		
+		var last = clean_neightbors.pop_front()
+		seen_neightbors.append(last)
+		print("New vector seen: ", last)
+	
+	return seen_neightbors
+
+func explode() -> void:
+	print("EXPLODE")
 
 func toggle_flag(tile_pos: Vector2i) -> void:
 	var x = tile_pos.x
